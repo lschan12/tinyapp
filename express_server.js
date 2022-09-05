4d40e8410667fcde62cbcd78f20e683a0041d291
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-09-05 15:40:35 -0700

    redirected post request from client side, generated tinyURL and then set a link to redirect to the webpage

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,18 +17,13 @@ const generateRandomString = () => {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
   for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random()) * characters.length);
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
@@ -42,15 +39,25 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
 
 
 app.listen(PORT, () => {
