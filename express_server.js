53592c368a93cd8dff770f0028384cdcb46830f2
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-09-05 14:58:02 -0700

    implemented a post request and created a function to generate a random string

--- express_server.js
+++ express_server.js
@@ -4,13 +4,29 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random()) * characters.length);
+  }
+  return result;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
@@ -22,6 +38,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
@@ -30,6 +50,8 @@ app.get("/urls/:id", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
